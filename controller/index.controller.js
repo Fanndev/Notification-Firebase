9938da9caaa75443c7723ed4d2b53b5d885a17a0
Genoschan1 <muhammadirfanfadhlurrahman@gmail.com>
2023-08-31 11:53:03 +0800

    Menambahkan refis

--- controller/index.controller.js
+++ controller/index.controller.js
@@ -1,80 +1,49 @@
-// with redis
-// const admin = require('firebase-admin');
-// const { initializeApp } = require("firebase-admin/app");
-// const express = require('express');
-// const cors = require('cors');
-// const Redis = require('ioredis');
+// with no redis
+// const express = require('express')
+// const FCM = require('fcm-node')
+// const SERVER_KEY = 'AAAA3dYMsTY:APA91bHCtvtywzKH8lH0vOK_7w77PoJXjqeXEGPdqI0SxZ_l5ST-c-AxV7FJTQaAalqP5hyalow8uGKli7YzkcqimuYv4sgt-thbP3wiLlA5eoNtxA1B6eEtZ0Gdh15N2gKCLx_StNtF '
 
 // const app = express();
+
 // const port = 3000;
-// app.use(express.json());
-
-// app.use(
-//   cors({
-//     origin: '*',
-//     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-//   })
-// );
-
-// app.use(function(req, res, next) {
-//   res.setHeader('Content-Type', 'application/json');
-//   next();
-// });
-
-// // Firebase initialization
-// const serviceAccount = require("./psychic-medley-354310-firebase-adminsdk-3i9cs-106fcd6742.json");
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   projectId: 'potion-for-creators',
-// });
-// const messaging = admin.messaging();
-
-// // Redis initialization
-// const redis = new Redis(); // Assumes Redis server is running on default host and port
-
-// // Queue processing function
-// async function processQueue() {
-//   while (true) {
-//     const token = await redis.lpop('notification_queue');
-//     if (!token) {
-//       break;
-//     }
-
-//     const message = {
-//       notification: {
-//         title: 'Notif',
-//         body: 'This is a Test Notification',
-//       },
-//       token: token,
-//     };
-
-//     try {
-//       const response = await messaging.send(message);
-//       console.log('Successfully sent message:', response);
-//     } catch (error) {
-//       console.log('Error sending message:', error);
-//     }
-//   }
-// }
-
-// app.post('/send', async function(req, res) {
-//   const receivedToken = req.body.fcmToken;
-  
-//   // Push token to the Redis queue
-//   await redis.rpush('notification_queue', receivedToken);
-  
-//   res.status(200).json({
-//     message: 'Message added to the queue',
-//     token: receivedToken,
-//   });
-// });
-
-// // Start processing the queue
-// processQueue();
 
 // app.listen(port, () => {
-//   console.log('Server started on port 3000');
-// });
+//   console.log('Listening port 3000')
+// })
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+
+// // Fcm 
+// app.post('/fcm', async(req, res, next) => {
+//   try {
+//       let fcm = new FCM(SERVER_KEY); 
+
+//       let message = {
+//         to : '/topics/' + req.body.topic,
+//         notification : {
+//           title : req.body.title,
+//           body : req.body.body,
+//           sound : 'default',
+//           "click_action" : "FCM_PLUGIN_ACTIVITY",
+//           "icon" : "fcm_icon_push"
+//         },
+//         data : req.body.data
+//       }
+
+//       fcm.send(message, (err, response) => {
+//         if (err) {
+//           next(err)
+//         }
+//         else {
+//           res.json(response)
+//         }
+//       })
+
+//   } catch (error) {
+    
+//   }
+// })
 
 // no redis
 // const admin = require('firebase-admin');
